238a31b820c6ade72e57642acf38818b6752c50d
Roberto <robertoabad93@gmail.com>
2021-03-12 13:14:35 +0100

    se generan varias respuestas

--- Responder.java
+++ Responder.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
+ * 
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
  * 
@@ -7,11 +11,21 @@
  */
 public class Responder
 {
+    private ArrayList<String> respuesta;
+    private int index;
+    Random r = new Random();
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        respuesta=new ArrayList<String>();
+        respuesta.add("si");
+        respuesta.add("no");
+        respuesta.add("tal vez");
+        respuesta.add("ni idea");
+        respuesta.add("exactamente");
+        respuesta.add("tiralo ganas dinero");
     }
 
     /**
@@ -20,6 +34,8 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int num= r.nextInt(respuesta.size());
+        String answer = respuesta.get(num);
+        return answer;
     }
 }
